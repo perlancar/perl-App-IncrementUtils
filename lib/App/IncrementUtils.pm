1e813c8a0d6dc54032c3c1db535efeed552b9cf7
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-25 16:46:23 +0700

    Initial commit

---
+++ lib/App/IncrementUtils.pm
@@ -0,0 +1,18 @@
+package App::IncrementUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Utilities related to increment/autoincrement
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
